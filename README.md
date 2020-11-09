fb69b8d50e87ca02db4635b57e1f57eeb4397968
Jeremy Sapsed <jeremy@sapsed.co.uk>
2020-11-09 19:42:15 +0000

    Created homebridge plugin for /api/v1 of light-controller

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# hb_rgb_light
+# Homebridge RGB Light
+
+A basic homebridge plugin used to integrate with a light exposed by an API with there required endpoints.
+
+## Expected Light Endpoints
+### Information (GET) *Required*
+Should return a json object with the following fields (note if not using the brightness or colour endpoints these can be ommitted):
+``` json
+{
+    "light": {
+        "switch": 0 or 1,
+        "brightness": number between 0 and 1,
+        "hsv": { "h": number, "s": number, "v": number (not used)}
+    }
+}
+```
+
+### Switch (PUT) *Required*
+Should expect a json body containing:
+``` json
+{
+    "switch": 0 or 1
+}
+```
+### Brightness (PUT) *Optional*
+Should expect a json body containing:
+``` json
+{
+    "brightness": number between 0 and 1,
+}
+```
+### Colour (PUT) *Optional*
+Should expect a json body containing:
+``` json
+{
+    "hsv": { "h": number, "s": number, "v": number (not used)}
+}
+```
---
+++ src/accessory.ts
@@ -0,0 +1,216 @@
+import Axios from "axios";
+import {
+  AccessoryConfig,
+  AccessoryPlugin,
+  API,
+  CharacteristicEventTypes,
+  CharacteristicGetCallback,
+  CharacteristicSetCallback,
+  CharacteristicValue,
+  HAP,
+  Logging,
+  Service,
+} from "homebridge";
+
+let hap: HAP;
+
+/*
+ * Initializer function called when the plugin is loaded.
+ */
+export = (api: API) => {
+  hap = api.hap;
+  api.registerAccessory("RGB-Light", RgbLight);
+};
+
+class RgbLight implements AccessoryPlugin {
+
+  private readonly log: Logging;
+  private readonly name: string;
+  private readonly infoUrl: string;
+  private readonly switchUrl: string;
+  private readonly brightnessUrl: string;
+  private readonly colourUrl: string;
+
+  private brightness: number;
+  private hue: number;
+  private saturation: number;
+
+  private readonly lightService: Service;
+  private readonly informationService: Service;
+
+  constructor(log: Logging, config: AccessoryConfig, api: API) {
+    this.log = log;
+    this.name = config.name;
+
+    this.brightness = 100;
+    this.hue = 0;
+    this.saturation = 0;
+
+    this.infoUrl = config.infoUrl;
+    this.switchUrl = config.switchUrl;
+    this.brightnessUrl = config.brightnessUrl;
+    this.colourUrl = config.colourUrl;
+
+    this.lightService = new hap.Service.Lightbulb(this.name);
+    this.lightService.getCharacteristic(hap.Characteristic.On)
+      .on(CharacteristicEventTypes.GET, this.handleOnGet.bind(this))
+      .on(CharacteristicEventTypes.SET, this.handleOnSet.bind(this));
+
+    if (this.brightnessUrl) {
+      this.lightService.getCharacteristic(hap.Characteristic.Brightness)
+        .on(CharacteristicEventTypes.GET, this.handleBrightnessGet.bind(this))
+        .on(CharacteristicEventTypes.SET, this.handleBrightnessSet.bind(this));
+    }
+
+    if (this.colourUrl) {
+      this.lightService.getCharacteristic(hap.Characteristic.Hue)
+        .on(CharacteristicEventTypes.GET, this.handleHueGet.bind(this))
+        .on(CharacteristicEventTypes.SET, this.handleHueSet.bind(this));
+
+      this.lightService.getCharacteristic(hap.Characteristic.Saturation)
+        .on(CharacteristicEventTypes.GET, this.handleSaturationGet.bind(this))
+        .on(CharacteristicEventTypes.SET, this.handleSaturationSet.bind(this));
+    }
+
+    this.informationService = new hap.Service.AccessoryInformation()
+      .setCharacteristic(hap.Characteristic.Manufacturer, "777 Productions")
+      .setCharacteristic(hap.Characteristic.Model, "Raspberry Pi - RGB LED Strip");
+
+    log.info("Switch finished initializing!");
+  }
+
+  handleOnGet(callback: CharacteristicGetCallback): void {
+    Axios.get(this.infoUrl)
+      .then((response) => {
+        let deviceInfo = response.data.light;
+        let state = deviceInfo.switch == 1;
+        this.log.info("Current state of the switch was returned: " + (state ? "ON" : "OFF"));
+        callback(undefined, state);
+      })
+      .catch((error) => {
+        this.log.error(`Failed to get on/off state. ${error}`);
+        callback(error);
+      });
+  }
+
+  handleOnSet(value: CharacteristicValue, callback: CharacteristicSetCallback): void {
+    Axios.put(this.switchUrl, {
+      switch: (value as boolean) ? 1 : 0
+    })
+      .then((response) => {
+        this.log.info(`Light state was set to: ${value as boolean ? "ON" : "OFF"}`);
+        callback();
+      })
+      .catch((error) => {
+        this.log.error(`Failed to turn light ${value as boolean ? "ON" : "OFF"}. ${error}`);
+        callback(error);
+      });
+  }
+
+  handleBrightnessGet(callback: CharacteristicGetCallback): void {
+    Axios.get(this.infoUrl)
+      .then((response) => {
+        let deviceInfo = response.data.light;
+        this.brightness = deviceInfo.brightness * 100;
+        this.log.info(`Current brightness returned: ${this.brightness}%`);
+        callback(undefined, this.brightness);
+      })
+      .catch((error) => {
+        this.log.error(`Failed to get brightness. ${error}`);
+        callback(error);
+      })
+  }
+
+  handleBrightnessSet(value: CharacteristicValue, callback: CharacteristicSetCallback): void {
+    Axios.put(this.brightnessUrl, { brightness: (value as number) / 100 })
+      .then((response) => {
+        this.log.info(`Light brighness was set to: ${value as number}`);
+        callback();
+      })
+      .catch((error) => {
+        this.log.error(`Failed to set brightness. ${error}`);
+      });
+  }
+
+  handleHueGet(callback: CharacteristicGetCallback): void {
+    Axios.get(this.infoUrl)
+      .then((response) => {
+        let deviceInfo = response.data.light;
+        let hsv = deviceInfo.hsv;
+        this.hue = hsv.h;
+        this.saturation = hsv.s;
+
+        this.log.info(`Current HSV: ${this.hue}, ${this.saturation}`);
+        callback(undefined, this.hue);
+      })
+      .catch((error) => {
+        this.log.error(`Failed to get hue. ${error}`);
+        callback(error);
+      });
+  }
+
+  handleHueSet(value: CharacteristicValue, callback: CharacteristicSetCallback): void {
+    this.hue = value as number;
+    let hsv = { h: this.hue, s: this.saturation, v: 100 };
+
+    Axios.put(this.colourUrl, { hsv: hsv })
+      .then((response) => {
+        this.log.info(`Light colour was set to: ${hsv}`);
+        callback();
+      })
+      .catch((error) => {
+        this.log.error(`Failed to set hue. ${error}`);
+      });
+  }
+
+  handleSaturationGet(callback: CharacteristicGetCallback): void {
+    Axios.get(this.infoUrl)
+      .then((response) => {
+        let deviceInfo = response.data.light;
+        let hsv = deviceInfo.hsv;
+        this.hue = hsv.h;
+        this.saturation = hsv.s;
+
+        this.log.info(`Current HSV: ${this.hue}, ${this.saturation}`);
+        callback(undefined, this.saturation);
+      })
+      .catch((error) => {
+        this.log.error(`Failed to get saturation. ${error}`);
+        callback(error);
+      });
+  }
+
+  handleSaturationSet(value: CharacteristicValue, callback: CharacteristicSetCallback): void {
+    this.saturation = value as number;
+    let hsv = { h: this.hue, s: this.saturation, v: 100 };
+    
+    Axios.put(this.colourUrl, { hsv: hsv })
+      .then((response) => {
+        this.log.info(`Light colour was set to: ${hsv}`);
+        callback();
+      })
+      .catch((error) => {
+        this.log.error(`Failed to set saturation. ${error}`);
+      });
+  }
+
+  /*
+   * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
+   * Typical this only ever happens at the pairing process.
+   */
+  identify(): void {
+    this.log("Identify!");
+  }
+
+  /*
+   * This method is called directly after creation of this instance.
+   * It should return all services which should be added to the accessory.
+   */
+  getServices(): Service[] {
+    return [
+      this.informationService,
+      this.lightService,
+    ];
+  }
+
+}
